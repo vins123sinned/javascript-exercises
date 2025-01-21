e7f7da72eaf49c37a3d19fc3e3d317500b2aeba8
Vinson <vinsonhe2@gmail.com>
2025-01-21 12:05:38 -0800

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    let newArray = array;
+    //arguments.length;
+    for (const item of toRemove) {
+        newArray = newArray.filter((current) => current !== item);
+    }
+    return newArray;
 };
 
 // Do not edit below this line
