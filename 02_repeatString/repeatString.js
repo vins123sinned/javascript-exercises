ad3ee40674c8f8ffcd74fb0b2f3eb13a5070622e
Vinson <vinsonhe2@gmail.com>
2025-01-21 11:09:54 -0800

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    if (times < 0) {
+        return 'ERROR';
+    }
+    
+    let result = '';
+    for (let i = 0; i < times; i++) {
+        result += string;
+    }
+    return result;
 };
 
 // Do not edit below this line
