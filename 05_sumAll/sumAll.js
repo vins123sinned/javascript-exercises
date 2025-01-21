a72a8175ba57b5e364ba4dc673633031d92564a0
Vinson <vinsonhe2@gmail.com>
2025-01-21 12:47:42 -0800

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(firstInput, secondInput) {
+    if (firstInput < 0 || secondInput < 0 ||
+      typeof(firstInput) !== 'number' || 
+      typeof(secondInput) !== 'number' ||
+      Number.isInteger(firstInput) !== true ||
+      Number.isInteger(secondInput) !== true) {
+        return 'ERROR';
+    }
 
+    let firstNumber;
+    let secondNumber;
+    let sum = 0;
+    
+    if (firstInput < secondInput) {
+        firstNumber = firstInput;
+        secondNumber = secondInput;
+    } else {
+        firstNumber = secondInput;
+        secondNumber = firstInput;
+    }
+
+    for (let i = firstNumber; i <= secondNumber; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
