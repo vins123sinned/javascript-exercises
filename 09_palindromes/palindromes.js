3dd8a5e0bd82dee74889a3117b17d1e734d8c7ca
Vinson <vinsonhe2@gmail.com>
2025-01-26 10:02:03 -0800

    Optimize Palindromes code once again

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,7 +2,7 @@ const palindromes = function (string) {
     const onlyLetters = string.replaceAll(/[^a-zA-Z0-9]+/g, '').toLowerCase();
     const reverseString = onlyLetters.split('').reverse().join('');
 
-    return onlyLetters === reverseString ? true: false;
+    return onlyLetters === reverseString;
 };
 
 // Do not edit below this line
