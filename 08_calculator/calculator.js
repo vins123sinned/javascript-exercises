8ad96d95022ee1f8259f7d3d7adc487253e2c72c
Vinson <vinsonhe2@gmail.com>
2025-01-26 09:23:42 -0800

    Complete exercise 08 Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, current) => product * current, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0) return 1;
+
+  let product = 1;
+  for (let i = number; i > 0; i--) {
+    product *= i;
+  }
+
+  return product;
 };
 
 // Do not edit below this line
