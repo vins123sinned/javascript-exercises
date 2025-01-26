cd4935cce06d47fa25d1c4904c563aeb2892f142
Vinson <vinsonhe2@gmail.com>
2025-01-26 09:59:22 -0800

    Complete exercise 09 Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const onlyLetters = string.replaceAll(/[^a-zA-Z0-9]+/g, '');
+    const reverseString = onlyLetters.toLowerCase().split('').reverse().join('');
 
+    return onlyLetters.toLowerCase() === reverseString ? true: false;
 };
 
 // Do not edit below this line
